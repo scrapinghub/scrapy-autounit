9c6e60a5fb5262dde5c5be58a79d172df0dd8977
Fabian Canobra <fabian@scrapinghub.com>
2019-04-23 15:54:16 -0300

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='scrapy-autounit',
-    version='0.0.13',
+    version='0.0.14',
     author='',
     author_email='',
     description='Automatic unit test generation for Scrapy.',
