92d08da4f2c2500d6a074fe007c4850044729dd2
Fabian Canobra <fabian@scrapinghub.com>
2020-07-27 15:41:28 -0300

    Add Python 2 classifier

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     url='https://github.com/scrapinghub/scrapy-autounit',
     packages=setuptools.find_packages(),
     classifiers=[
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
